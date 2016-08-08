828c34193c6ae751d2945ac16da3f0e4189384eb
Jon Clayden <code@clayden.org>
2016-08-08 13:25:13 +0100

    Adding xform, dim, pixdim code

---
+++ R/image.R
@@ -0,0 +1,194 @@
+#' Internal images
+#' 
+#' An internal image is a simple R object with a few attributes including a
+#' pointer to an internal C structure, which contains the full image data. They
+#' are used in the package for efficiency, but can be converted to a normal
+#' R array using the \code{as.array} method. Attributes of these objects should
+#' not be changed.
+#' 
+#' @param x An \code{"internalImage"} object.
+#' @param value Not used. Changing the dimensions of an internal image is
+#'   invalid, and will produce an error.
+#' @param ... Additional parameters to methods. Currently unused.
+#' 
+#' @author Jon Clayden <code@@clayden.org>
+#' @aliases internalImage
+#' @rdname internalImage
+#' @export
+dim.internalImage <- function (x)
+{
+    return (attr(x, "imagedim"))
+}
+
+
+#' @rdname internalImage
+#' @export
+"dim<-.internalImage" <- function (x, value)
+{
+    stop("Dimensions of an internal image cannot be changed")
+}
+
+
+#' @rdname internalImage
+#' @export
+as.array.internalImage <- function (x, ...)
+{
+    return (.Call("pointerToArray", x, PACKAGE="RNifti"))
+}
+
+
+#' @export
+print.niftiImage <- function (x, ...)
+{
+    dim <- dim(x)
+    ndim <- length(dim)
+    pixdim <- attr(x, "pixdim")
+    pixunits <- attr(x, "pixunits")
+    
+    if ("internalImage" %in% class(x))
+        cat(paste0("Internal image: \"", x, "\"\n"))
+    else
+        cat(paste0("Image array of mode \"", storage.mode(x), "\" (", format(object.size(x),"auto"), ")\n"))
+    
+    cat(paste("-", paste(dim,collapse=" x "), ifelse(ndim>2,"voxels\n","pixels\n")))
+    
+    if (!is.null(pixdim))
+    {
+        spaceUnit <- grep("m$", pixunits, perl=TRUE, value=TRUE)
+        cat(paste("-", paste(signif(pixdim[1:min(3,ndim)],4),collapse=" x ")))
+        if (length(spaceUnit) > 0)
+            cat(paste0(" ", spaceUnit[1]))
+        
+        if (ndim > 3)
+        {
+            timeUnit <- grep("s$", pixunits, perl=TRUE, value=TRUE)
+            cat(paste(" x", signif(pixdim[4],4)))
+            if (length(timeUnit) > 0)
+                cat(paste0(" ", timeUnit[1]))
+        }
+        if (ndim > 4)
+            cat(paste(" x", paste(signif(pixdim[5:ndim],4),collapse=" x ")))
+        
+        cat(paste(" per", ifelse(ndim>2,"voxel\n","pixel\n")))
+    }
+}
+
+
+#' Number of dimensions
+#' 
+#' This function is shorthand for \code{length(dim(object))}.
+#' 
+#' @param object An R object.
+#' @return The dimensionality of the object. Objects without a \code{dim}
+#'   attribute will produce zero.
+#' 
+#' @author Jon Clayden <code@@clayden.org>
+#' @export
+ndim <- function (object)
+{
+    length(dim(object))
+}
+
+
+#' Pixel dimensions and units
+#' 
+#' By default, these generic functions return or replace the \code{"pixdim"}
+#' and \code{"pixunits"} attributes of their arguments. These represent the
+#' physical step size between pixel or voxel centre points, and the spatial and
+#' temporal units that they are given in. The former defaults to 1 in each
+#' dimension, if there is no attribute.
+#' 
+#' @param object An R object, generally an image.
+#' @param value Numeric vector of pixel dimensions along each axis, or
+#'   character vector of abbreviated units. For dimensions, a scalar
+#'   \code{value} will be recycled if necessary.
+#' @return \code{pixdim} returns a numeric vector of pixel dimensions.
+#'   \code{pixunits} returns a character vector of length up to two, giving the
+#'   spatial and temporal unit names.
+#' 
+#' @author Jon Clayden <code@@clayden.org>
+#' @export
+pixdim <- function (object)
+{
+    UseMethod("pixdim")
+}
+
+
+#' @rdname pixdim
+#' @export
+pixdim.default <- function (object)
+{
+    if (!is.null(attr(object, "pixdim")))
+        return (attr(object, "pixdim"))
+    else if (!is.null(dim(object)))
+        return (rep(1, length(dim(object))))
+    else
+        return (1)
+}
+
+
+#' @rdname pixdim
+#' @export
+"pixdim<-" <- function (object, value)
+{
+    UseMethod("pixdim<-")
+}
+
+
+#' @rdname pixdim
+#' @export
+"pixdim<-.default" <- function (object, value)
+{
+    if ("internalImage" %in% class(object))
+        stop("Pixel dimensions of an internal image cannot be changed")
+    
+    if (is.numeric(value))
+    {
+        if (length(value) == ndim(object))
+            attr(object, "pixdim") <- value
+        else if (length(value) == 1)
+            attr(object, "pixdim") <- rep(value, ndim(object))
+    }
+    return (object)
+}
+
+
+#' @rdname pixdim
+#' @export
+pixunits <- function (object)
+{
+    UseMethod("pixunits")
+}
+
+
+#' @rdname pixdim
+#' @export
+pixunits.default <- function (object)
+{
+    if (!is.null(attr(object, "pixunits")))
+        return (attr(object, "pixunits"))
+    else
+        return ("Unknown")
+}
+
+
+#' @rdname pixdim
+#' @export
+"pixunits<-" <- function (object, value)
+{
+    UseMethod("pixunits<-")
+}
+
+
+#' @rdname pixdim
+#' @export
+"pixunits<-.default" <- function (object, value)
+{
+    if ("internalImage" %in% class(object))
+        stop("Pixel units of an internal image cannot be changed")
+    
+    if (is.character(value))
+        attr(object, "pixunits") <- value
+    
+    return (object)
+}
---
+++ R/xform.R
@@ -0,0 +1,122 @@
+#' Obtain or replace the ``xform'' transforms for an image
+#' 
+#' These functions convert the ``qform'' or ``sform'' information in a NIfTI
+#' header to or from a corresponding affine matrix. These two ``xform''
+#' mechanisms are defined by the NIfTI standard, and may both be in use in a
+#' particular image header.
+#' 
+#' @param image,x An image, in any acceptable form (see \code{\link{isImage}}).
+#' @param useQuaternionFirst A single logical value. If \code{TRUE}, the
+#'   ``qform'' matrix will be used first, if it is defined; otherwise the
+#'   ``sform'' matrix will take priority.
+#' @param value A new 4x4 qform or sform matrix. If the matrix has a
+#'   \code{"code"} attribute, the appropriate qform or sform code is also set.
+#' @return A affine matrix corresponding to the ``qform'' or ``sform''
+#'   information in the image header. This is a plain matrix, which does not
+#'   have the \code{"affine"} class or \code{source} and \code{target}
+#'   attributes.
+#' 
+#' @note The qform and sform replacement functions are for advanced users only.
+#'   Modifying the transforms without knowing what you're doing is usually
+#'   unwise, as you can make the image object inconsistent.
+#' 
+#' @author Jon Clayden <code@@clayden.org>
+#' @references The NIfTI-1 standard (\url{http://nifti.nimh.nih.gov/nifti-1})
+#'   is the definitive reference on ``xform'' conventions.
+#' @export
+xform <- function (image, useQuaternionFirst = TRUE)
+{
+    return (.Call("getXform", image, isTRUE(useQuaternionFirst), PACKAGE="RNifti"))
+}
+
+
+#' @rdname xform
+#' @export
+"qform<-" <- function (x, value)
+{
+    return (.Call("setXform", x, value, TRUE, PACKAGE="RNifti"))
+}
+
+
+#' @rdname xform
+#' @export
+"sform<-" <- function (x, value)
+{
+    return (.Call("setXform", x, value, FALSE, PACKAGE="RNifti"))
+}
+
+
+#' Transform points between voxel and ``world'' coordinates
+#' 
+#' These functions are used to transform points from dimensionless pixel or
+#' voxel coordinates to ``real-world'' coordinates, typically in millimetres,
+#' and back. Actual pixel units can be obtained using the
+#' \code{\link{pixunits}} function.
+#' 
+#' @param points A vector giving the coordinates of a point, or a matrix with
+#'   one point per row.
+#' @param image The image in whose space the points are given.
+#' @param simple A logical value: if \code{TRUE} then the transformation is
+#'   performed simply by rescaling the points according to the voxel dimensions
+#'   recorded in the \code{image}. Otherwise the full xform matrix is used.
+#' @param ... Additional arguments to \code{\link{xform}}.
+#' @return A vector or matrix of transformed points.
+#' 
+#' @note Voxel coordinates are assumed by these functions to use R's indexing
+#'   convention, beginning from 1.
+#' 
+#' @author Jon Clayden <code@@clayden.org>
+#' @seealso \code{\link{xform}}, \code{\link{pixdim}}, \code{\link{pixunits}}
+#' @export
+voxelToWorld <- function (points, image, simple = FALSE, ...)
+{
+    if (simple)
+    {
+        if (!is.matrix(points))
+            points <- matrix(points, nrow=1)
+        voxelDims <- pixdim(image)[seq_len(ncol(points))]
+        return (drop(t(apply(points-1, 1, function(x) x*abs(voxelDims)))))
+    }
+    else
+    {
+        if (!is.matrix(points))
+            points <- matrix(points, nrow=1)
+        affine <- xform(image, ...)
+        
+        nDims <- ncol(points)
+        if (nDims != 3)
+            stop("Points must be three-dimensional")
+        
+        points <- cbind(points-1, 1)
+        newPoints <- affine %*% t(points)
+        return (drop(t(newPoints[1:nDims,,drop=FALSE])))
+    }
+}
+
+
+#' @rdname voxelToWorld
+#' @export
+worldToVoxel <- function (points, image, simple = FALSE, ...)
+{
+    if (simple)
+    {
+        if (!is.matrix(points))
+            points <- matrix(points, nrow=1)
+        voxelDims <- pixdim(image)[seq_len(ncol(points))]
+        return (drop(t(apply(points, 1, function(x) x/abs(voxelDims)) + 1)))
+    }
+    else
+    {
+        if (!is.matrix(points))
+            points <- matrix(points, nrow=1)
+        affine <- solve(xform(image, ...))
+        
+        nDims <- ncol(points)
+        if (nDims != 3)
+            stop("Points must be three-dimensional")
+        
+        points <- cbind(points, 1)
+        newPoints <- affine %*% t(points) + 1
+        return (drop(t(newPoints[1:nDims,,drop=FALSE])))
+    }
+}
