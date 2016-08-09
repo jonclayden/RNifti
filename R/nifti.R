59db532e1e1d016e893bdf157a5c03b7feb293bd
Jon Clayden <code@clayden.org>
2016-08-09 17:12:45 +0100

    Dropping source and target arguments to readNifti()

--- R/nifti.R
+++ R/nifti.R
@@ -4,10 +4,6 @@
 #' NIfTI-1 C library.
 #' 
 #' @param file A character vector of file names.
-#' @param source,target If the specified \code{file} contains a transformation,
-#'   these parameters can be used to specify the associated source and target
-#'   images, which are stored in attributes of the same name. Only used if
-#'   \code{file} is of unit length.
 #' @param internal Logical value. If \code{FALSE} (the default), an array
 #'   of class \code{"niftiImage"}, containing the image pixel or voxel values,
 #'   will be returned. If \code{TRUE}, the return value will be an object of
@@ -21,7 +17,7 @@
 #' @seealso \code{\link{writeNifti}}
 #' @references The NIfTI-1 standard (\url{http://nifti.nimh.nih.gov/nifti-1}).
 #' @export
-readNifti <- function (file, source = NULL, target = NULL, internal = FALSE)
+readNifti <- function (file, internal = FALSE)
 {
     if (!is.character(file))
         stop("File name(s) must be specified in a character vector")
@@ -30,14 +26,7 @@ readNifti <- function (file, source = NULL, target = NULL, internal = FALSE)
     else if (length(file) > 1)
         lapply(file, function(f) .Call("readNifti", path.expand(f), internal, PACKAGE="RNifti"))
     else
-    {
-        image <- .Call("readNifti", path.expand(file), internal, PACKAGE="RNifti")
-        if (!is.null(source))
-            attr(image, "source") <- .Call("retrieveImage", source, PACKAGE="RNifti")
-        if (!is.null(target))
-            attr(image, "target") <- .Call("retrieveImage", target, PACKAGE="RNifti")
-        return (image)
-    }
+        .Call("readNifti", path.expand(file), internal, PACKAGE="RNifti")
 }
 
 #' Write a NIfTI-1 format file
