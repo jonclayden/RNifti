95eefe322e236b0003c625febab62a44db196dbd
Jon Clayden <code@clayden.org>
2016-09-16 12:53:54 +0100

    Adding OpenMP critical regions to avoid multithreaded calls to R API

--- inst/include/RNiftiAPI.h
+++ inst/include/RNiftiAPI.h
@@ -12,6 +12,9 @@ extern "C" {
 nifti_1_header * nifti_make_new_header (const int arg_dims[], int arg_dtype)
 {
     static nifti_1_header*(*fun)(const int*, int) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (nifti_1_header*(*)(const int*, int)) R_GetCCallable("RNifti","nii_make_new_header");
     return fun(arg_dims, arg_dtype);
@@ -20,6 +23,9 @@ nifti_1_header * nifti_make_new_header (const int arg_dims[], int arg_dtype)
 nifti_image * nifti_make_new_nim (const int dims[], int datatype, int data_fill)
 {
     static nifti_image*(*fun)(const int*, int, int) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (nifti_image*(*)(const int*, int, int)) R_GetCCallable("RNifti","nii_make_new_nim");
     return fun(dims, datatype, data_fill);
@@ -28,6 +34,9 @@ nifti_image * nifti_make_new_nim (const int dims[], int datatype, int data_fill)
 nifti_image * nifti_convert_nhdr2nim (struct nifti_1_header nhdr, const char *fname)
 {
     static nifti_image*(*fun)(struct nifti_1_header, const char *) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (nifti_image*(*)(struct nifti_1_header, const char *)) R_GetCCallable("RNifti","nii_convert_nhdr2nim");
     return fun(nhdr, fname);
@@ -36,6 +45,9 @@ nifti_image * nifti_convert_nhdr2nim (struct nifti_1_header nhdr, const char *fn
 struct nifti_1_header nifti_convert_nim2nhdr (const nifti_image *nim)
 {
     static struct nifti_1_header(*fun)(const nifti_image *) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (struct nifti_1_header(*)(const nifti_image *)) R_GetCCallable("RNifti","nii_convert_nim2nhdr");
     return fun(nim);
@@ -44,6 +56,9 @@ struct nifti_1_header nifti_convert_nim2nhdr (const nifti_image *nim)
 nifti_image * nifti_copy_nim_info (const nifti_image *src)
 {
     static nifti_image*(*fun)(const nifti_image*) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (nifti_image*(*)(const nifti_image*)) R_GetCCallable("RNifti","nii_copy_nim_info");
     return fun(src);
@@ -52,6 +67,9 @@ nifti_image * nifti_copy_nim_info (const nifti_image *src)
 int nifti_copy_extensions (nifti_image *nim_dest, const nifti_image *nim_src)
 {
     static int(*fun)(nifti_image*, const nifti_image*) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (int(*)(nifti_image*, const nifti_image*)) R_GetCCallable("RNifti","nii_copy_extensions");
     return fun(nim_dest, nim_src);
@@ -60,6 +78,9 @@ int nifti_copy_extensions (nifti_image *nim_dest, const nifti_image *nim_src)
 void nifti_image_free (nifti_image *nim)
 {
     static void(*fun)(nifti_image*) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (void(*)(nifti_image*)) R_GetCCallable("RNifti","nii_image_free");
     fun(nim);
@@ -68,6 +89,9 @@ void nifti_image_free (nifti_image *nim)
 void nifti_datatype_sizes (int datatype, int *nbyper, int *swapsize)
 {
     static void(*fun)(int, int*, int*) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (void(*)(int, int*, int*)) R_GetCCallable("RNifti","nii_datatype_sizes");
     return fun(datatype, nbyper, swapsize);
@@ -76,6 +100,9 @@ void nifti_datatype_sizes (int datatype, int *nbyper, int *swapsize)
 char const * nifti_datatype_string (int dt)
 {
     static char const *(*fun)(int) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (char const *(*)(int)) R_GetCCallable("RNifti","nii_datatype_string");
     return fun(dt);
@@ -84,6 +111,9 @@ char const * nifti_datatype_string (int dt)
 char const * nifti_units_string (int uu)
 {
     static char const *(*fun)(int) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (char const *(*)(int)) R_GetCCallable("RNifti","nii_units_string");
     return fun(uu);
@@ -92,6 +122,9 @@ char const * nifti_units_string (int uu)
 size_t nifti_get_volsize (const nifti_image *nim)
 {
     static size_t(*fun)(const nifti_image *) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (size_t(*)(const nifti_image *)) R_GetCCallable("RNifti","nii_get_volsize");
     return fun(nim);
@@ -100,6 +133,9 @@ size_t nifti_get_volsize (const nifti_image *nim)
 int nifti_update_dims_from_array (nifti_image *nim)
 {
     static int(*fun)(nifti_image *) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (int(*)(nifti_image *)) R_GetCCallable("RNifti","nii_update_dims_from_array");
     return fun(nim);
@@ -108,6 +144,9 @@ int nifti_update_dims_from_array (nifti_image *nim)
 int nifti_set_filenames (nifti_image *nim, const char *prefix, int check, int set_byte_order)
 {
     static int(*fun)(nifti_image*, const char*, int, int) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (int(*)(nifti_image*, const char*, int, int)) R_GetCCallable("RNifti","nii_set_filenames");
     return fun(nim, prefix, check, set_byte_order);
@@ -116,6 +155,9 @@ int nifti_set_filenames (nifti_image *nim, const char *prefix, int check, int se
 nifti_image * nifti_image_read (const char *hname, int read_data)
 {
     static nifti_image *(*fun)(const char*, int) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (nifti_image *(*)(const char*, int)) R_GetCCallable("RNifti","nii_image_read");
     return fun(hname, read_data);
@@ -124,6 +166,9 @@ nifti_image * nifti_image_read (const char *hname, int read_data)
 void nifti_image_write (nifti_image *nim)
 {
     static void(*fun)(nifti_image *) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (void(*)(nifti_image *)) R_GetCCallable("RNifti","nii_image_write");
     fun(nim);
@@ -132,6 +177,9 @@ void nifti_image_write (nifti_image *nim)
 float nifti_mat33_determ (mat33 R)
 {
     static float(*fun)(mat33) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (float(*)(mat33)) R_GetCCallable("RNifti","nii_mat33_determ");
     return fun(R);
@@ -140,6 +188,9 @@ float nifti_mat33_determ (mat33 R)
 mat33 nifti_mat33_inverse (mat33 R)
 {
     static mat33(*fun)(mat33) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (mat33(*)(mat33)) R_GetCCallable("RNifti","nii_mat33_inverse");
     return fun(R);
@@ -148,6 +199,9 @@ mat33 nifti_mat33_inverse (mat33 R)
 mat33 nifti_mat33_mul (mat33 A, mat33 B)
 {
     static mat33(*fun)(mat33, mat33) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (mat33(*)(mat33, mat33)) R_GetCCallable("RNifti","nii_mat33_mul");
     return fun(A, B);
@@ -156,6 +210,9 @@ mat33 nifti_mat33_mul (mat33 A, mat33 B)
 mat33 nifti_mat33_polar (mat33 A)
 {
     static mat33(*fun)(mat33) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (mat33(*)(mat33)) R_GetCCallable("RNifti","nii_mat33_polar");
     return fun(A);
@@ -164,6 +221,9 @@ mat33 nifti_mat33_polar (mat33 A)
 mat44 nifti_mat44_inverse (mat44 R)
 {
     static mat44(*fun)(mat44) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (mat44(*)(mat44)) R_GetCCallable("RNifti","nii_mat44_inverse");
     return fun(R);
@@ -172,6 +232,9 @@ mat44 nifti_mat44_inverse (mat44 R)
 void nifti_mat44_to_quatern (mat44 R, float *qb, float *qc, float *qd, float *qx, float *qy, float *qz, float *dx, float *dy, float *dz, float *qfac)
 {
     static void(*fun)(mat44, float *, float *, float *, float *, float *, float *, float *, float *, float *, float *) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (void(*)(mat44, float *, float *, float *, float *, float *, float *, float *, float *, float *, float *)) R_GetCCallable("RNifti","nii_mat44_to_quatern");
     fun(R, qb, qc, qd, qx, qy, qz, dx, dy, dz, qfac);
@@ -180,6 +243,9 @@ void nifti_mat44_to_quatern (mat44 R, float *qb, float *qc, float *qd, float *qx
 mat44 nifti_quatern_to_mat44 (float qb, float qc, float qd, float qx, float qy, float qz, float dx, float dy, float dz, float qfac)
 {
     static mat44(*fun)(float, float, float, float, float, float, float, float, float, float) = NULL;
+#ifdef _OPENMP
+#pragma omp critical
+#endif
     if (fun == NULL)
         fun = (mat44(*)(float, float, float, float, float, float, float, float, float, float)) R_GetCCallable("RNifti","nii_quatern_to_mat44");
     return fun(qb, qc, qd, qx, qy, qz, dx, dy, dz, qfac);
