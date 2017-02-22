a1cb282a0196f927136cc93abd3f277ea7741fbd
Jon Clayden <code@clayden.org>
2017-02-22 22:09:54 +0000

    Adding tests that look at the data and rescale an image

--- tests/testthat/test-05-nifti.R
+++ tests/testthat/test-05-nifti.R
@@ -27,4 +27,11 @@ test_that("NIfTI files can be read and written", {
     
     image <- updateNifti(image, list(intent_code=1000L))
     expect_that(dumpNifti(image)$intent_code, equals(1000L))
+    
+    expect_that(image[40,40,30], equals(368))
+    image <- readNifti(imagePath, internal=TRUE)
+    expect_that(as.array(image)[40,40,30], equals(368))
+    image <- RNifti:::rescaleNifti(image, c(0.5,0.5,0.5))
+    expect_that(pixdim(image), equals(c(5,5,5)))
+    expect_that(as.array(image), gives_warning("no data"))
 })
