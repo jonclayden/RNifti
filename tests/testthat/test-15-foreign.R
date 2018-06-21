747a59acc6b4d1e76237da61a243cc7c2354b91b
Jon Clayden <code@clayden.org>
2018-06-21 20:19:34 +0200

    Improving usage of testthat

--- tests/testthat/test-15-foreign.R
+++ tests/testthat/test-15-foreign.R
@@ -1,27 +1,25 @@
 context("Working with objects from other packages")
 
-test_that("image objects from other packages can be read", {
+test_that("image objects from oro.nifti can be read", {
     imagePath <- system.file("extdata", "example.nii.gz", package="RNifti")
     
-    if (system.file(package="oro.nifti") == "")
-        skip("The \"oro.nifti\" package is not available")
-    else
-    {
-        # The oro.nifti package warns about nonzero slope, which is nothing to worry about
-        image <- suppressWarnings(oro.nifti::readNIfTI(imagePath))
-        expect_that(dumpNifti(image)$bitpix, equals(32L))
-    }
+    skip_if_not_installed("oro.nifti")
     
-    if (system.file(package="tractor.base") == "")
-        skip("The \"tractor.base\" package is not available")
-    else
-    {
-        reportr::setOutputLevel(reportr::OL$Warning)
-        
-        image <- tractor.base::readImageFile(imagePath)
-        expect_that(dumpNifti(image)$bitpix, equals(32L))
-        
-        image <- tractor.base::readImageFile(imagePath, sparse=TRUE)
-        expect_that(dumpNifti(image)$bitpix, equals(32L))
-    }
+    # The oro.nifti package warns about nonzero slope, which is nothing to worry about
+    image <- suppressWarnings(oro.nifti::readNIfTI(imagePath))
+    expect_that(dumpNifti(image)$bitpix, equals(32L))
+})
+
+test_that("image objects from tractor.base can be read", {
+    imagePath <- system.file("extdata", "example.nii.gz", package="RNifti")
+    
+    skip_if_not_installed("tractor.base")
+    
+    reportr::setOutputLevel(reportr::OL$Warning)
+    
+    image <- tractor.base::readImageFile(imagePath)
+    expect_that(dumpNifti(image)$bitpix, equals(32L))
+    
+    image <- tractor.base::readImageFile(imagePath, sparse=TRUE)
+    expect_that(dumpNifti(image)$bitpix, equals(32L))
 })
