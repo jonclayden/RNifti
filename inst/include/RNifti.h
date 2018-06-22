f7e9066f042986beef0733395f0aa284b7cceee5
Jon Clayden <code@clayden.org>
2018-06-22 13:18:50 +0100

    Also accept HAVE_R, which is currently used by RNiftyReg and divest

--- inst/include/RNifti.h
+++ inst/include/RNifti.h
@@ -1,6 +1,11 @@
 #ifndef _RNIFTI_H_
 #define _RNIFTI_H_
 
+// RNiftyReg and divest have used HAVE_R, so accept this variant for compatibility
+#if !defined(USING_R) && defined(HAVE_R)
+#define USING_R
+#endif
+
 #include "niftilib/nifti1_io.h"
 
 #ifdef __cplusplus
