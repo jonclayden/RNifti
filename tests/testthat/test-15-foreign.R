90e06cb67e37c379e1d2181f6c2f557215f7d32e
Jon Clayden <code@clayden.org>
2019-06-17 12:51:50 +0100

    Adding tests for indexing into foreign-derived internal images

--- tests/testthat/test-15-foreign.R
+++ tests/testthat/test-15-foreign.R
@@ -8,6 +8,7 @@ test_that("image objects from oro.nifti can be read", {
     # The oro.nifti package warns about nonzero slope, which is nothing to worry about
     image <- suppressWarnings(oro.nifti::readNIfTI(imagePath))
     expect_equal(niftiHeader(image)$bitpix, 32L)
+    expect_equal(retrieveNifti(image)[40,40,30], 368)
 })
 
 test_that("image objects from tractor.base can be read", {
@@ -20,7 +21,9 @@ test_that("image objects from tractor.base can be read", {
     # NB: the $ operator shortcut can't be used since "image" isn't a niftiImage
     image <- tractor.base::readImageFile(imagePath)
     expect_equal(niftiHeader(image)$bitpix, 32L)
+    expect_equal(retrieveNifti(image)[40,40,30], 368)
     
     image <- tractor.base::readImageFile(imagePath, sparse=TRUE)
     expect_equal(niftiHeader(image)$bitpix, 32L)
+    expect_equal(retrieveNifti(image)[40,40,30], 368)
 })
