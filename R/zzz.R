487a8e83e4f4587c300e2773a885d2a568a09dc1
Jon Clayden <code@clayden.org>
2019-11-12 15:19:42 +0000

    Adding missing namespace imports

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,7 @@
-#' @importFrom grDevices col2rgb
+#' @import graphics
+#' @import grDevices
 #' @importFrom Rcpp evalCpp
+#' @importFrom stats quantile
 #' @importFrom utils object.size
 #' @useDynLib RNifti
 NULL
