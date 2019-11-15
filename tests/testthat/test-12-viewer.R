ad76fb61d2b4c6ecb429a288c8d0f236e4abf77d
Jon Clayden <code@clayden.org>
2019-11-15 09:34:32 +0000

    Adding viewer tests

---
+++ tests/testthat/test-12-viewer.R
@@ -0,0 +1,11 @@
+context("Displaying images")
+
+test_that("greyscale and RGB images can be viewed", {
+    expect_null(view(system.file("extdata", "example.nii.gz", package="RNifti"), interactive=FALSE))
+    image <- readNifti(system.file("extdata", "example.nii.gz", package="RNifti"))
+    expect_message(view(layer(image,max=NA), interactive=FALSE), "(0, 748)")
+    rgbImage <- readNifti(system.file("extdata", "example_rgb.nii.gz", package="RNifti"))
+    expect_equal(rgbImage$datatype, 128L)
+    expect_null(view(rgbImage, interactive=FALSE))
+    grDevices::dev.off()
+})
