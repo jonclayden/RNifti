88efb9bd8ae1109614f6335089e60123ce380eae
Jon Clayden <code@clayden.org>
2020-04-24 16:35:31 +0100

    Refactoring API generator, since use of niftilib v2-only types (for example) needs to be fenced

--- scripts/generateAPI.R
+++ scripts/generateAPI.R
@@ -2,55 +2,66 @@
 
 library(ore)
 
-values <- symbols <- argPieces <- NULL
+versions <- NULL
 
-for (file in file.path("inst","include","niftilib",c("nifti1_io.h","nifti2_io.h"))) {
-    lines <- readLines(file)
+for (version in 1:2) {
+    # Read header file containing function declarations
+    lines <- readLines(file.path("inst", "include", "niftilib", es("nifti#{version}_io.h")))
     
+    # Unwrap continuation lines
     wrapped <- which(lines %~% "^[^\\(]+\\)")
     lines[wrapped-1] <- paste(lines[wrapped-1], lines[wrapped])
     lines <- lines[-wrapped]
     
+    # Pattern-match each declaration, identifying groups corresponding to the
+    # return type, function name and function arguments
     pieces <- groups(ore.search("^(\\w[\\w\\s]+?\\**)\\s*(\\w+)\\s*\\(([^\\)]+)\\)\\s*;\\s*$", lines))
-    values <- c(values, ore.subst("^\\s+(.+?)\\s+", "\\1", pieces[,1,]))
-    symbols <- c(symbols, pieces[,2,])
+    values <- ore.subst("^\\s+(.+?)\\s+", "\\1", pieces[,1,])
+    values <- ore.subst("\\s+(\\s)", "\\1", values, all=TRUE)
+    symbols <- pieces[,2,]
     
+    # Remove leading and trailing whitespace from the arg list, split at commas
+    # and find the type and name of each argument
     args <- ore.subst("^\\s*(.+?)\\s*$", "\\1", pieces[,3,])
     args <- ore.subst("\\s+([\\s,])", "\\1", args, all=TRUE)
-    argPieces <- c(argPieces, groups(ore.search("(\\Avoid\\Z)|\\b([\\w\\s]+?\\**)\\s*(\\w+)\\s*(\\[\\])?(,|\\Z)", args, all=TRUE)))
+    argPieces <- groups(ore.search("(\\Avoid\\Z)|\\b([\\w\\s]+?\\**)\\s*(\\w+)\\s*(\\[\\])?(,|\\Z)", args, all=TRUE))
+    
+    # Create shortened names for the R-internal callable functions
+    abbreviatedSymbols <- ore.subst("nifti", "nii", symbols)
+    
+    # Reconstruct the argument list: types only, names only and in full
+    voidType <- sapply(argPieces, function(x) isTRUE(!is.na(x[,1])))
+    argTypes <- ifelse(voidType, "void", NA_character_)
+    argTypes[!voidType] <- sapply(argPieces[!voidType], function(x) paste(x[,2],collapse=", "))
+    argNames <- sapply(argPieces, function(x) paste(x[,3],collapse=", "))
+    args <- sapply(argPieces, function(x) paste(x[,2],x[,3],collapse=", "))
+    
+    # Construct code fragments required for each function
+    registerCallables <- paste0("R_RegisterCCallable(\"RNifti\", \"", abbreviatedSymbols, "\", (DL_FUNC) &", symbols, ");")
+    declarePointers <- paste0("static ", values, "(*_", symbols, ")(", argTypes, ") = NULL;")
+    mapPointers <- paste0("_", symbols, " = (", values, "(*)(", argTypes, ")) R_GetCCallable(\"RNifti\", \"", abbreviatedSymbols, "\");")
+    
+    # Construct wrapper functions
+    defineWrappers <- rep(NA, length(argPieces))
+    defineWrappers[voidType] <- paste0(values[voidType], " ", symbols[voidType], " () { NIFTILIB_WRAPPER_BODY_VOID(_", symbols[voidType], ") }")
+    defineWrappers[!voidType] <- paste0(values[!voidType], " ", symbols[!voidType], " (", args[!voidType], ") { NIFTILIB_WRAPPER_BODY(_", symbols[!voidType], ", ", argNames[!voidType], ") }")
+    defineWrappers <- na.omit(defineWrappers)
+    
+    versions <- c(versions, list(list(registerCallables=registerCallables, declarePointers=declarePointers, mapPointers=mapPointers, defineWrappers=defineWrappers)))
 }
 
-values <- ore.subst("\\s+(\\s)", "\\1", values, all=TRUE)
-
-duplicated <- duplicated(symbols)
-abbreviatedSymbols <- ore.subst("nifti", "nii", symbols)
-voidType <- sapply(argPieces, function(x) isTRUE(!is.na(x[,1])))
-argTypes <- ifelse(voidType, "void", NA_character_)
-argTypes[!voidType] <- sapply(argPieces[!voidType], function(x) paste(x[,2],collapse=", "))
-argNames <- sapply(argPieces, function(x) paste(x[,3],collapse=", "))
-args <- sapply(argPieces, function(x) paste(x[,2],x[,3],collapse=", "))
-
-registerCallables <- paste0("R_RegisterCCallable(\"RNifti\", \"", abbreviatedSymbols[!duplicated], "\", (DL_FUNC) &", symbols[!duplicated], ");")
-declarePointers <- paste0("static ", values[!duplicated], "(*_", symbols[!duplicated], ")(", argTypes[!duplicated], ") = NULL;")
-mapPointers <- paste0("_", symbols[!duplicated], " = (", values[!duplicated], "(*)(", argTypes[!duplicated], ")) R_GetCCallable(\"RNifti\", \"", abbreviatedSymbols[!duplicated], "\");")
-
-defineWrappers <- rep(NA, length(argPieces))
-defineWrappers[voidType & !duplicated] <- paste0(values[voidType & !duplicated], " ", symbols[voidType & !duplicated], " () { NIFTILIB_WRAPPER_BODY_VOID(_", symbols[voidType & !duplicated], ") }")
-defineWrappers[!voidType & !duplicated] <- paste0(values[!voidType & !duplicated], " ", symbols[!voidType & !duplicated], " (", args[!voidType & !duplicated], ") { NIFTILIB_WRAPPER_BODY(_", symbols[!voidType & !duplicated], ", ", argNames[!voidType & !duplicated], ") }")
-defineWrappers <- na.omit(defineWrappers)
-
-replacePlaceholder <- function (file, labels, strings) {
+replacePlaceholder <- function (file, labels, vars) {
     lines <- readLines(paste0(file, ".in"))
     for (i in seq_along(labels)) {
         mark <- which(lines %~% es("^(\\s*)/\\* MARK - #{labels[i]} \\*/"))
         if (length(mark) == 1) {
             padding <- groups()
-            strings[[i]] <- paste0(ifelse(is.na(padding),"",padding), strings[[i]])
-            lines <- c(lines[seq_len(mark-1)], strings[[i]], lines[-seq_len(mark)])
+            strings <- lapply(1:2, function(version) paste0(ifelse(is.na(padding),"",padding), versions[[version]][[vars[i]]]))
+            lines <- c(lines[seq_len(mark-1)], "#if RNIFTI_NIFTILIB_VERSION == 1", strings[[1]], "#elif RNIFTI_NIFTILIB_VERSION == 2", strings[[2]], "#endif", lines[-seq_len(mark)])
         }
     }
     writeLines(lines, file)
 }
 
-replacePlaceholder(file.path("src","main.cpp"), "Register callables", list(registerCallables))
-replacePlaceholder(file.path("inst","include","RNiftiAPI.h"), c("Declare pointers", "Map pointers", "Define wrappers"), list(declarePointers, mapPointers, defineWrappers))
+replacePlaceholder(file.path("src","main.cpp"), "Register callables", "registerCallables")
+replacePlaceholder(file.path("inst","include","RNiftiAPI.h"), c("Declare pointers", "Map pointers", "Define wrappers"), c("declarePointers", "mapPointers", "defineWrappers"))
