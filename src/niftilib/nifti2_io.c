f2db586e48b14a23a817019eebc64eca451a39dd
Jon Clayden <code@clayden.org>
2020-08-14 00:45:34 +0100

    Patching niftilib v2 so that it actually produces valid NIfTI-2 files

--- src/niftilib/nifti2_io.c
+++ src/niftilib/nifti2_io.c
@@ -3823,6 +3823,7 @@ char * nifti_findimgname(const char* fname , int nifti_type)
       /* if we get 3 or more extensions, can make a loop here... */
 
       if (nifti_type == NIFTI_FTYPE_NIFTI1_1) first = 0; /* should match .nii */
+      else if (nifti_type == NIFTI_FTYPE_NIFTI2_1) first = 0;
       else                                    first = 1; /* should match .img */
 
       strcpy(imgname,basename);
@@ -3904,6 +3905,7 @@ char * nifti_makehdrname(const char * prefix, int nifti_type, int check,
    }
    /* otherwise, make one up */
    else if( nifti_type == NIFTI_FTYPE_NIFTI1_1 ) strcat(iname, extnii);
+   else if( nifti_type == NIFTI_FTYPE_NIFTI2_1 ) strcat(iname, extnii);
    else if( nifti_type == NIFTI_FTYPE_ASCII )    strcat(iname, extnia);
    else                                          strcat(iname, exthdr);
 
@@ -3978,6 +3980,7 @@ char * nifti_makeimgname(const char * prefix, int nifti_type, int check,
    }
    /* otherwise, make one up */
    else if( nifti_type == NIFTI_FTYPE_NIFTI1_1 ) strcat(iname, extnii);
+   else if( nifti_type == NIFTI_FTYPE_NIFTI2_1 ) strcat(iname, extnii);
    else if( nifti_type == NIFTI_FTYPE_ASCII )    strcat(iname, extnia);
    else                                          strcat(iname, extimg);
 
@@ -4116,7 +4119,8 @@ int nifti2_type_and_names_match( nifti_image * nim, int show_warn )
    if( errs ) return 0;   /* do not proceed, but this is just a mis-match */
 
    /* general tests */
-   if( nim->nifti_type == NIFTI_FTYPE_NIFTI1_1 ){  /* .nii */
+   if( (nim->nifti_type == NIFTI_FTYPE_NIFTI1_1) ||
+       (nim->nifti_type == NIFTI_FTYPE_NIFTI2_1) ){  /* .nii */
       if( fileext_n_compare(ext_h,".nii",4) ) {
          if( show_warn )
             Rc_fprintf_stderr(
@@ -4140,6 +4144,7 @@ int nifti2_type_and_names_match( nifti_image * nim, int show_warn )
       }
    }
    else if( (nim->nifti_type == NIFTI_FTYPE_NIFTI1_2) || /* .hdr/.img */
+            (nim->nifti_type == NIFTI_FTYPE_NIFTI2_2) ||
             (nim->nifti_type == NIFTI_FTYPE_ANALYZE) )
    {
       if( fileext_n_compare(ext_h,".hdr",4) != 0 ){
@@ -4372,9 +4377,11 @@ int nifti2_set_type_from_names( nifti_image * nim )
    } else {
       /* not too picky here, do what must be done, and then verify */
       if( strcmp(nim->fname, nim->iname) == 0 )          /* one file, type 1 */
-         nim->nifti_type = NIFTI_FTYPE_NIFTI1_1;
+         nim->nifti_type = (nim->nifti_type >= NIFTI_FTYPE_NIFTI2_1) ? NIFTI_FTYPE_NIFTI2_1 : NIFTI_FTYPE_NIFTI1_1;
       else if( nim->nifti_type == NIFTI_FTYPE_NIFTI1_1 ) /* cannot be type 1 */
          nim->nifti_type = NIFTI_FTYPE_NIFTI1_2;
+      else if( nim->nifti_type == NIFTI_FTYPE_NIFTI2_1 )
+         nim->nifti_type = NIFTI_FTYPE_NIFTI2_2;
    }
 
    if( g_opts.debug > 2 ) Rc_fprintf_stderr(" -> %d\n",nim->nifti_type);
@@ -5013,7 +5020,7 @@ nifti_image* nifti_convert_n2hdr2nim(nifti_2_header nhdr, const char * fname)
 
    is_onefile = (ni_ver > 0) && NIFTI_ONEFILE(nhdr) ;
 
-   nim->nifti_type = (is_onefile) ? NIFTI_FTYPE_NIFTI1_1 : NIFTI_FTYPE_NIFTI1_2;
+   nim->nifti_type = (is_onefile) ? NIFTI_FTYPE_NIFTI2_1 : NIFTI_FTYPE_NIFTI2_2;
 
    ii = nifti_short_order() ;
    if( doswap )   nim->byteorder = REVERSE_ORDER(ii) ;
@@ -7519,8 +7526,8 @@ int nifti_convert_nim2n2hdr(const nifti_image * nim, nifti_2_header * hdr)
    /**- load the ANALYZE-7.5 generic parts of the header struct */
 
    nhdr.sizeof_hdr = sizeof(nhdr) ;
-   if( nim->nifti_type == NIFTI_FTYPE_NIFTI2_1 ) strcpy(nhdr.magic,"n+2") ;
-   else                                          strcpy(nhdr.magic,"ni2") ;
+   memcpy(nhdr.magic, nifti2_magic, 8);
+   if( nim->nifti_type == NIFTI_FTYPE_NIFTI2_2 ) nhdr.magic[1] = 'i';
 
    nhdr.datatype = nim->datatype ;
    nhdr.bitpix   = 8 * nim->nbyper ;
@@ -7731,6 +7738,7 @@ void nifti2_set_iname_offset(nifti_image *nim, int nifti_ver)
 
      /* NIFTI-1 single binary file - always update */
      case NIFTI_FTYPE_NIFTI1_1:
+     case NIFTI_FTYPE_NIFTI2_1:
        offset = nifti_extension_size(nim) + hsize + 4;
        /* be sure offset is aligned to a 16 byte boundary */
        if ( ( offset % 16 ) != 0 )  offset = ((offset + 0xf) & ~0xf);
@@ -7821,8 +7829,6 @@ znzFile nifti2_image_write_hdr_img2(nifti_image *nim, int write_opts,
    if( write_data && NBL && ! nifti_NBL_matches_nim(nim, NBL) )
       ERREX("NBL does not match nim");
 
-   nifti_set_iname_offset(nim, 1);
-
    if( g_opts.debug > 1 ){
       Rc_fprintf_stderr("-d writing nifti file '%s'...\n", nim->fname);
       if( g_opts.debug > 2 )
@@ -7832,21 +7838,19 @@ znzFile nifti2_image_write_hdr_img2(nifti_image *nim, int write_opts,
 
    if( nim->nifti_type == NIFTI_FTYPE_ASCII )   /* non-standard case */
       return nifti_write_ascii_image(nim,NBL,opts,write_data,leave_open);
-
-   /* create the nifti header struct                  5 Aug, 2015 [rickr]
-        - default is NIFTI-1 (option?)
-        - if that fails try NIFTI-2
-   */
-   if( nifti_convert_nim2n1hdr(nim, &n1hdr) ) {
+   else if( nim->nifti_type == NIFTI_FTYPE_NIFTI2_1 || nim->nifti_type == NIFTI_FTYPE_NIFTI2_2 ) {
       nifti_set_iname_offset(nim, 2);
       if( nifti_convert_nim2n2hdr(nim, &n2hdr) ) return NULL;
-      Rc_fprintf_stderr("+d writing %s as NIFTI-2, instead...\n", nim->fname);
-      nver = 2; /* we will write NIFTI-2 */
+      nver = 2;
       hsize = (int)sizeof(nifti_2_header);
+   }
+   else {
+      nifti_set_iname_offset(nim, 1);
+      if( nifti_convert_nim2n1hdr(nim, &n1hdr) ) return NULL;
    }
 
    /* if writing to 2 files, make sure iname is set and different from fname */
-   if( nim->nifti_type != NIFTI_FTYPE_NIFTI1_1 ){
+   if( (nim->nifti_type != NIFTI_FTYPE_NIFTI1_1) && (nim->nifti_type != NIFTI_FTYPE_NIFTI2_1) ){
        if( nim->iname && strcmp(nim->iname,nim->fname) == 0 ){
          free(nim->iname) ; nim->iname = NULL ;
        }
@@ -7857,7 +7861,7 @@ znzFile nifti2_image_write_hdr_img2(nifti_image *nim, int write_opts,
    }
 
    /* if we have an imgfile and will write the header there, use it */
-   if( ! znz_isnull(imgfile) && nim->nifti_type == NIFTI_FTYPE_NIFTI1_1 ){
+   if( ! znz_isnull(imgfile) && (nim->nifti_type == NIFTI_FTYPE_NIFTI1_1 || nim->nifti_type == NIFTI_FTYPE_NIFTI2_1) ){
       if( g_opts.debug > 2 ) Rc_fprintf_stderr("+d using passed file for hdr\n");
       fp = imgfile;
    }
@@ -7891,7 +7895,7 @@ znzFile nifti2_image_write_hdr_img2(nifti_image *nim, int write_opts,
       znzclose(fp); return(fp);
    }
 
-   if( nim->nifti_type != NIFTI_FTYPE_NIFTI1_1 ){ /* get a new file pointer */
+   if( (nim->nifti_type != NIFTI_FTYPE_NIFTI1_1) && (nim->nifti_type != NIFTI_FTYPE_NIFTI2_1) ){ /* get a new file pointer */
       znzclose(fp);         /* first, close header file */
       if( ! znz_isnull(imgfile) ){
          if(g_opts.debug > 2) Rc_fprintf_stderr("+d using passed file for img\n");
@@ -8224,6 +8228,8 @@ char *nifti2_image_to_ascii( const nifti_image *nim )
               (nim->nifti_type == NIFTI_FTYPE_NIFTI1_1) ? "NIFTI-1+"
              :(nim->nifti_type == NIFTI_FTYPE_NIFTI1_2) ? "NIFTI-1"
              :(nim->nifti_type == NIFTI_FTYPE_ASCII   ) ? "NIFTI-1A"
+             :(nim->nifti_type == NIFTI_FTYPE_NIFTI2_1) ? "NIFTI-2+"
+             :(nim->nifti_type == NIFTI_FTYPE_NIFTI2_2) ? "NIFTI-2"
              :                         "ANALYZE-7.5" ) ;
 
    /** Strings that we don't control (filenames, etc.) that might
@@ -8571,6 +8577,10 @@ nifti_image *nifti2_image_from_ascii( const char *str, int * bytes_read )
                nim->nifti_type = NIFTI_FTYPE_NIFTI1_2 ;
        else if( strcmp(rhs,"NIFTI-1A")    == 0 )
                nim->nifti_type = NIFTI_FTYPE_ASCII ;
+       else if( strcmp(rhs,"NIFTI-2+")    == 0 )
+               nim->nifti_type = NIFTI_FTYPE_NIFTI2_1 ;
+       else if( strcmp(rhs,"NIFTI-2")     == 0 )
+               nim->nifti_type = NIFTI_FTYPE_NIFTI2_2 ;
      }
      else if( strcmp(lhs,"header_filename") == 0 ){
        nim->fname = nifti_strdup(rhs) ;
