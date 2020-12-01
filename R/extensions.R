9a39616b3133d1579c3719b1cea74846c2f01b72
Jon Clayden <code@clayden.org>
2020-12-01 12:25:29 +0000

    Using a simple named vector for extension codes; documenting it

--- R/extensions.R
+++ R/extensions.R
@@ -1,12 +1,14 @@
+#' NIfTI extension codes
+#' 
 #' @export
-ExtensionCodes <- list(ignore=0L, DICOM=2L, AFNI=4L, comment=6L, XCEDE=8L, jimdiminfo=10L, workflow_fwds=12L, FreeSurfer=14L, pypickle=16L, MiND_ident=18L, b_value=20L, spherical_direction=22L, DT_component=24L, SHC_degreeorder=26L, voxbo=28L, Caret=30L, CIFTI=32L, variable_frame_timing=34L, eval=38L, MATLAB=40L, Quantiphyse=42L, MRS=44L)
+ExtensionCodes <- c(ignore=0L, DICOM=2L, AFNI=4L, comment=6L, XCEDE=8L, jimdiminfo=10L, workflow_fwds=12L, FreeSurfer=14L, pypickle=16L, MiND_ident=18L, b_value=20L, spherical_direction=22L, DT_component=24L, SHC_degreeorder=26L, voxbo=28L, Caret=30L, CIFTI=32L, variable_frame_timing=34L, eval=38L, MATLAB=40L, Quantiphyse=42L, MRS=44L)
 
 .resolveCode <- function (code)
 {
     name <- as.character(substitute(code, parent.frame()))
     loc <- match(tolower(name), tolower(names(ExtensionCodes)))
     if (length(name) == 1L && !is.na(loc))
-        return (ExtensionCodes[[loc]])
+        return (ExtensionCodes[loc])
     else if (is.numeric(code) && code < 0)
         stop("Extension code must not be negative")
     else
@@ -26,7 +28,8 @@ ExtensionCodes <- list(ignore=0L, DICOM=2L, AFNI=4L, comment=6L, XCEDE=8L, jimdi
 #' corresponding code in an attribute, and the replacement form accepts a list
 #' of atomic vectors with code attributes, or \code{NULL}, which removes all
 #' extensions. The singular version, \code{extension}, gets all extensions with
-#' the specified code, or appends an extension with that code.
+#' the specified code, or appends an extension with that code. Valid extension
+#' codes are stored in the \code{\link{ExtensionCodes}} vector.
 #' 
 #' NIfTI extensions are stored as a simple, unstructured byte stream, which is
 #' naturally represented in R as a vector of mode \code{"raw"}. However, these
@@ -36,7 +39,8 @@ ExtensionCodes <- list(ignore=0L, DICOM=2L, AFNI=4L, comment=6L, XCEDE=8L, jimdi
 #' be passed through when converting to a non-raw type.
 #' 
 #' @param image An image, in any acceptable form (see \code{\link{asNifti}}).
-#' @param code Integer value specifying which extension code is required.
+#' @param code Integer value, expression or string specifying which extension
+#'   code is required.
 #' @param mode The required mode of the extracted data.
 #' @param ... Additional arguments to \code{\link{readBin}}.
 #' @param simplify Logical value. If \code{TRUE}, the default, a single
@@ -49,10 +53,8 @@ ExtensionCodes <- list(ignore=0L, DICOM=2L, AFNI=4L, comment=6L, XCEDE=8L, jimdi
 #'   specified. If the extension code is not used in the image, the return
 #'   value is \code{NULL}. The replacement forms return the modified image.
 #' 
-#' @note A list of registered extension codes is available in the
-#'   \code{nifti2_io.h} header file, which is distributed with this package.
-#' 
 #' @author Jon Clayden <code@@clayden.org>
+#' @seealso \code{\link{ExtensionCodes}} for the valid extension codes.
 #' @export
 extensions <- function (image)
 {
