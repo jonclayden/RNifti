3e29a27ab7a4b6dd228e9435523acec9a11f6649
Jon Clayden <code@clayden.org>
2021-05-12 22:31:30 +0100

    Fencing out body of nifti2_image_to_ascii() in R for the moment, given portability question mark

--- src/niftilib/nifti2_io.c
+++ src/niftilib/nifti2_io.c
@@ -8209,6 +8209,10 @@ static char *escapize_string( const char * str )
 *//*-------------------------------------------------------------------------*/
 char *nifti2_image_to_ascii( const nifti_image *nim )
 {
+#ifdef USING_R
+   Rf_error("nifti2_image_to_ascii is currently unimplemented for R packages, for portability reasons");
+   return NULL;
+#else
    char *buf , *ebuf ; int nbuf ;
 
    if( nim == NULL ) return NULL ;   /* stupid caller */
@@ -8451,6 +8455,7 @@ char *nifti2_image_to_ascii( const nifti_image *nim )
    if( !buf ) Rc_fprintf_stderr("** NIFTI NITA: failed to realloc %d bytes\n",
                       nbuf+1);
    return buf ;
+#endif
 }
 
 /*---------------------------------------------------------------------------*/
