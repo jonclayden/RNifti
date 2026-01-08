a41f0f645018f9ccb291ae1ad54c383f1fa19f1d
Jon Clayden <code@clayden.org>
2026-01-08 21:03:20 +0000

    Add summarise fuzz tests with integer values

--- inst/tinytest/test-55-fuzz.R
+++ inst/tinytest/test-55-fuzz.R
@@ -23,3 +23,11 @@ expect_equal(sum(is.na(image)), 1)
 expect_equal(mean(image,na.rm=TRUE), mean(data,na.rm=TRUE))
 expect_equal(range(image,na.rm=TRUE), range(data,na.rm=TRUE))
 expect_equal(range(image+1,data,na.rm=TRUE), c(min(data,na.rm=TRUE), max(data,na.rm=TRUE)+1))
+
+# Similar with integer-mode data (since NA_integer_ is not reflected naturally in C/C++)
+data <- matrix(sample(-5:15,9), 3, 3)
+data[sample(1:9,1)] <- NA_integer_
+image <- asNifti(data, internal=TRUE)
+expect_equal(sum(image), NA_integer_)
+expect_equal(sum(image,na.rm=TRUE), sum(data,na.rm=TRUE))
+expect_equal(range(image,na.rm=TRUE), range(data,na.rm=TRUE))
